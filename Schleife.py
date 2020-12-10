a60a0ba477ebf20db4b069490a4d95f6f31891fe
Pirmin <pzd3mir@googlemail.com>
2020-12-11 00:18:09 +0100

    Calculates Cross Sum

---
+++ Schleife.py
@@ -0,0 +1,10 @@
+
+"""Zählen Sie mit Hilfe einer Schleife (nicht mit der Gaussschen Summenformel) alle Zahlen von 1 bis n zusammen und geben Sie das Endergebnis aus."""
+
+x = int(input())
+y = int(0)
+z = int(0)
+while (x >= y):
+    z = z + y
+    y = y + 1
+print(z)
