655a0371e34e77ffc8d3e31df7c565065d3383b5
Pirmin <pzd3mir@googlemail.com>
2020-12-11 13:47:48 +0100

    Reads Numbers from a file into an array, converts them into an int array and sorts them.

---
+++ KPW_hs2020_Uebung4/BinaryDecimal.py
@@ -0,0 +1,15 @@
+"""Es soll eine Binärzahl in eine Dezimalzahl konvertiert werden. Lesen Sie eine Folge von Nullen und Einsen ein und wandeln
+Sie diese Binärzahl in die entsprechende Dezimalzahl um."""
+
+# -*- coding: utf-8 -*-
+# @author: Pirmin Özdemir
+
+x = input()
+
+list  = x.split()
+print(x)
+
+
+
+#def binarytoDecimal():
+
